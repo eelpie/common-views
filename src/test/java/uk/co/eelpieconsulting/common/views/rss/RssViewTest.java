389f65ca93392c0b3c6b63d337eda29fa9a4ed1e
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-03-09 08:05:22 +0000

    Test georss output.

--- src/test/java/uk/co/eelpieconsulting/common/views/rss/RssViewTest.java
+++ src/test/java/uk/co/eelpieconsulting/common/views/rss/RssViewTest.java
@@ -1,9 +1,14 @@
 package uk.co.eelpieconsulting.common.views.rss;
 
+import com.rometools.modules.georss.GeoRSSModule;
+import com.rometools.modules.georss.SimpleModuleImpl;
+import com.rometools.modules.georss.geometries.Position;
+import com.rometools.rome.feed.synd.SyndEntryImpl;
 import org.junit.Test;
 import uk.co.eelpieconsulting.common.views.EtagGenerator;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 
 import static org.junit.Assert.assertTrue;
 
@@ -13,9 +18,36 @@ public class RssViewTest {
     public void canRenderRssView() {
         RssView rssView = new RssView(new EtagGenerator(), "A feed", "http://localhost/feed", "Just another feed");
 
-        String rendered = rssView.renderRss(new ArrayList());
+        SyndEntryImpl entry = new SyndEntryImpl();
+        entry.setTitle("An item");
+        entry.setLink("http://localhost/an-item");
+        entry.setPublishedDate(new Date());
+
+        String rendered = rssView.renderRss(Arrays.asList(entry));
 
         assertTrue(rendered.contains("<title>A feed</title>"));
+        assertTrue(rendered.contains("<title>An item</title>"));
+    }
+
+    @Test
+    public void canRenderGeoRSSItems() {
+        RssView rssView = new RssView(new EtagGenerator(), "A feed", "http://localhost/feed", "Just another feed");
+
+        SyndEntryImpl entry = new SyndEntryImpl();
+        entry.setTitle("A geo item");
+        entry.setLink("http://localhost/a-geo-item");
+        entry.setPublishedDate(new Date());
+
+        final GeoRSSModule geoRSSModule = new SimpleModuleImpl();
+        Position position = new Position(51.1, -0.3);
+        geoRSSModule.setPosition(position);
+        geoRSSModule.setFeatureNameTag("Some where");
+        entry.getModules().add(geoRSSModule);
+
+        String rendered = rssView.renderRss(Arrays.asList(entry));
+
+        assertTrue(rendered.contains("<title>A feed</title>"));
+        assertTrue(rendered.contains("<georss:point>51.1 -0.3</georss:point>"));
     }
 
 }
